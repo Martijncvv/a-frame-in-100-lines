61344dea8394c581bc49b97b5229486cf4b22ec6
martijn <martijncvv@gmail.com>
2024-04-24 03:18:09 +0200

    - dynamic bg 8

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const frameMetadata = getFrameMetadata({
 
 
   image: {
-    src: `${NEXT_PUBLIC_URL}/api/background-image/state=`,
+    src: `${NEXT_PUBLIC_URL}/mastermind-1.png`,
     aspectRatio: '1:1',
   },
   postUrl: `${NEXT_PUBLIC_URL}/api/gameplay_easy`,
